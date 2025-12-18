5eabdc2ba785a93025fcfa3d26b628d88973f64e
Gian0sa <giepeton558@gmail.com>
2025-12-18 13:55:46 -0500

    "backend"

--- backend/Tour_Viajes_Hospe/SlnTour_Transporte_Hospedaje/ProyTour_Transporte_Hospedaje/obj/Debug/net7.0/ProyTour_Transporte_Hospedaje.AssemblyInfo.cs
+++ backend/Tour_Viajes_Hospe/SlnTour_Transporte_Hospedaje/ProyTour_Transporte_Hospedaje/obj/Debug/net7.0/ProyTour_Transporte_Hospedaje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyTour_Transporte_Hospedaje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed399c9bf6c62de20a5c611781f2088e9f480579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33f88f98170e10a3aa67cd9efe9e891551a67b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyTour_Transporte_Hospedaje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyTour_Transporte_Hospedaje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
